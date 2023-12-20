f3bb3e2b0a3e9c4cdea8378fbca861c7e5960fb9
gulnarhasanzada <gulnar.hesenzade@gmail.com>
2023-12-19 23:00:55 -0500

    Adding signup, login routes and updating navigation in header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
 import {RouterProvider, createBrowserRouter} from "react-router-dom";
-import HomePage from './pages/HomePage';
+import Home from './pages/Home';
 import Layout from './pages/Layout';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 const router = createBrowserRouter([
   {path: "/", element: <Layout/>, children: [
-      {path: "/", element: <HomePage/>}
+      {path: "/", element: <Home/>},
+      {path: "/signup", element: <Signup/>},
+      {path: "/login", element: <Login/>},
    ]
   }
 ]);
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import { FaUserPlus } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
 import "./header.css"
 
 const Header = ()=>{
@@ -5,13 +8,14 @@ const Header = ()=>{
         <header className="header-container">
             <div className="header-main  d-flex">
                 <div className="header-item header-logo">
-                    
+                    <Link to="/"><img src="/excursia-logo.png" alt="excursia-logo" title="Excursia"/></Link>
                 </div>
                 <div className="header-item">
 
                 </div>
-                <div className="header-item header-profile">
-                    
+                <div className="header-item header-auth">       
+                    <Link to="/login" className="auth-link login"><FaUserCircle size="23" className="auth-icon"/>Login</Link>
+                    <Link to="/signup" className="auth-link signup"><FaUserPlus size="23" className="auth-icon"/>Signup</Link>
                 </div>
             </div>
         </header>
